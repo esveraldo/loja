9e2e7d47d4b348d2834e4c8699f7edb29d3b06fc
esveraldo <esveraldo@hotmail.com>
2018-01-07 16:41:52 -0200

    Configurado arquivo app.php

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://teste-esveraldo.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
@@ -103,7 +103,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:EmQTbnJKORnJWIyvsq5S57T3SNO56/4B1MJFoE7V5NU='),
 
     'cipher' => 'AES-256-CBC',
 
